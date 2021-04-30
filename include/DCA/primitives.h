0a9b787b12e0fa222abd6f973d5ccf744d4e89da
Matthias Busenhart <busenham@student.ethz.ch>
2021-04-30 17:54:00 +0200

    Add constructor for primitives

--- include/DCA/primitives.h
+++ include/DCA/primitives.h
@@ -9,23 +9,27 @@
 #ifndef __DCA_PRIMITIVES_H__
 #define __DCA_PRIMITIVES_H__
 
-#include "utils.h"
 #include <variant>
+
+#include "utils.h"
 
 namespace DCA {
 
-    struct Sphere {
-        Vector3d position;
-        double radius;
-    };
+struct Sphere {
+    Sphere(const Vector3d& pos, const double& r) : position(pos), radius(r) {}
+    Vector3d position;
+    double radius;
+};
 
-    struct Capsule {
-        Vector3d startPosition;
-        Vector3d endPosition;
-        double radius;
-    };
+struct Capsule {
+    Capsule(const Vector3d& startPos, const Vector3d& endPos, const double& r)
+        : startPosition(startPos), endPosition(endPos), radius(r) {}
+    Vector3d startPosition;
+    Vector3d endPosition;
+    double radius;
+};
 
-    using primitive_t = std::variant<Sphere, Capsule>;
+using primitive_t = std::variant<Sphere, Capsule>;
 
 }  // namespace DCA
 #endif /* __DCA_PRIMITIVES_H__ */
